d2e9b7c2f507e12bcf0dc848912d0c05ae1504eb
Fran <fm-a@robindatahood.com>
2024-07-14 18:44:36 +0200

    started home

--- app/site2/page.tsx
+++ app/site2/page.tsx
@@ -1,28 +1,36 @@
-import {
-    disciplines,
-    howCanHelp,
-    homeParagraph,
-    heroText,
-    academicOfferings,
-    campuses,
-    chiefPresentation,
-  } from "@/lib/data.js";
-  
-  import React from "react";
-  import HeroSection from "@/components/sections/HeroSection";
-  import ParagraphSection from "@/components/sections/ParagraphSection";
-  
-  import ImgListSection from "@/components/sections/ImgListSection";
-  import ListArraySection from "@/components/sections/ListArraySection";
-  import ArticleRevList from "@/components/sections/ArticleRevList";
-  import GridList from "@/components/sections/GridList";
-  import HeroFullWSection from "@/components/sections/HeroFullWSection";
+import Image from "next/image";
+import {homeHero } from "@/lib/dataClinique"
+import Link from "next/link";
   
   export default function Home() {
     return (
       <main className="bg-grey_light">
+
+        <section className="relative bg-verde_oscuro w-full h-screen flex flex-row items-center">
+
+          <div className="flex flex-col gap-y-8 absolute">
+            <div className="text-white">{homeHero.title1}</div>
+            <div className="text-verde h1c">{homeHero.title2}</div>
+            <Link className="btnC" href={'#'} >{homeHero.btnText}</Link>
+
+          </div>
+
+          <div className=" flex justify-end">
        
-        <ParagraphSection text={homeParagraph} />
+       <Image
+          // className=" pb-[190px]lg:pb-0 mx-auto flex 2xl:max-w-[740px] xl:max-w-[624px] lg:max-w-[455px] "
+          className=" w-full h-screen object-cover object-top
+          lg:w-[50%] "
+
+          src={homeHero.image}
+          alt="logo"
+          width={740}
+          height={777}
+
+       
+        />
+        </div>
+        </section>
   
       </main>
     );
--- components/FooterCli.tsx
+++ components/FooterCli.tsx
@@ -1,77 +1,89 @@
 import React from "react";
-import { fAddresse,fHeures,fReferer,fPourNousJoindre } from "@/lib/dataClinique";
+import {
+  fAddresse,
+  fHeures,
+  fReferer,
+  fPourNousJoindre,
+} from "@/lib/dataClinique";
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "@/public/img/clinique/parolanaudiere-logoblanc.svg";
 
 export default function FooterCli() {
- 
   return (
-    <footer className="section flex flex-col  bg-verde_oscuro text-white  text-lg">
-    
-      <section className="flex flex-col lg:flex-row gap-y-[90px] 
+    <footer className=" flex flex-col  bg-verde_oscuro text-white  text-lg">
+      <section
+        className="py-[142px] lg:py-[110px] flex flex-col lg:flex-row gap-y-[90px] 
                         px-10 
 	                  sm:max-w-[407px] sm:px-0 
 					  md:max-w-[588px]
 					  lg:max-w-none lg:px-[90px]
 					  2xl:max-w-[1228px] 2xl:px-0
-      ">
-      <Link href="/site2" className="">
-        <Image
-          // className='w-[188px] h-[90px]'
-          className="  "
-          src={Logo}
-          alt="logo"
-          width={210}
-           height={65}
-        />
-      </Link>
-     <div className="flex flex-col">
-      <div className="h3c">{fAddresse.title}</div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
-        {/* adresse */}
-         <div>
-            <div>{fAddresse.adresse.title}</div>
+            border-2 border-white
+      "
+      >
+        <Link href="/site2" className="flex mx-auto border-2 border-white">
+          <Image
+            className=" 
+                       w-[368px] h-[120px] object-cover 
+                       sm:w-[532px] sm:h-[165px]           "
+            src={Logo}
+            alt="logo"
+            width={210}
+            height={65}
+          />
+        </Link>
+        <div className="flex flex-col">
+          <div className="h3c text-verde">{fAddresse.title}</div>
+          <div className="grid grid-cols-1 lg:grid-rows-3  gap-5">
+            {/* adresse */}
             <div>
-              {fAddresse.adresse.addresse.map(item => (
-                <p key={item}>{item}</p>
-              ))}
+              <div className="text-verde mb-3">{fAddresse.adresse.title}</div>
+              <div>
+                {fAddresse.adresse.addresse.map((item) => (
+                  <p key={item}>{item}</p>
+                ))}
+              </div>
+              <Link href={fAddresse.mapsAddress}>{fAddresse.mapsLegend}</Link>
             </div>
-            <Link href={fAddresse.mapsAddress}>{fAddresse.mapsLegend}</Link>
-         </div>
-          {/* nous joindre */}
-          <div>
-            <div>{fPourNousJoindre.title}</div>
-            <div>{fPourNousJoindre.phone}</div>
-           <div>{fPourNousJoindre.email}</div>
-         </div>
-
-         {/* heures */}
-         <div>
-            <div>{fHeures.title}</div>
+            {/* nous joindre */}
             <div>
-              {fHeures.items.map(item => (
-                <p key={item}>{item}</p>
-              ))}
+              <div className="text-verde mb-3">{fPourNousJoindre.title}</div>
+              <div className="flex flex-col mb-12">
+                <a href={`tel: +${fPourNousJoindre.phone} `}>
+                  {fPourNousJoindre.phone}
+                </a>
+                <a href={`mailto:${fPourNousJoindre.email} `}>
+                  {fPourNousJoindre.email}
+                </a>
+              </div>
             </div>
-            <Link href={fAddresse.mapsAddress}>{fAddresse.mapsLegend}</Link>
-         </div>
 
-          {/* referer */}
-          <div>
-            <div>{fReferer.title}</div>
-            <div>{fReferer.text}</div>
-           <Link href={"#"} >{fReferer.btn}</Link>
-         </div>
+            {/* heures */}
+            <div>
+              <div className="text-verde mb-3">{fHeures.title}</div>
+              <div className="mb-12">
+                {fHeures.items.map((item) => (
+                  <p key={item}>{item}</p>
+                ))}
+              </div>
+            
+            </div>
 
-
-      </div>
-     </div>
+            {/* referer */}
+            <div>
+              <div className="text-verde mb-3">{fReferer.title}</div>
+              <div className="mb-12">{fReferer.text}</div>
+              <Link className="btnC" href={"#"}>{fReferer.btn}</Link>
+            </div>
+          </div>
+        </div>
       </section>
 
-
       <section className="flex">
-        <p className="items-center mx-auto pt-5">2024 Pruinboom Institute / Terms & conditions and cookies policy</p>
+        <p className="items-center mx-auto pt-5">
+          2024 Pruinboom Institute / Terms & conditions and cookies policy
+        </p>
       </section>
     </footer>
   );
